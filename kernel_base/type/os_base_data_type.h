c99add7de45729fffc7d72b6b51bfb63bc54aba5
xiongluo <lxhuster_dev@sina.com>
2015-12-09 06:36:34 +0800

    add kernel base type define

---
+++ kernel_base/type/os_base_data_type.h
@@ -0,0 +1,25 @@
+/*
+   define OS data type
+ */
+
+/* bit confirm unsigned data type define */
+typedef unsigned char u8_t;
+typedef unsigned short u16_t;
+typedef unsigned int u32_t;
+typedef unsigned long long u64_t;
+
+/* bit confirm signed data type define */
+typedef char s8_t;
+typedef short s16_t;
+typedef int s32_t;
+typedef long long s64_t;
+
+/* normal type */
+typedef long sint_t;
+typedef unsigned long uint_t;
+
+/* pointer type define */
+typedef void * p_buf_t;
+
+/* regist type define */
+typedef u32_t regist_t;
